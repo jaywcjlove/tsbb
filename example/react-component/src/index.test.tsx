beb4b7dee615285def1d8370152d95a5d0700f90
jaywcjlove <398188662@qq.com>
2019-06-04 01:56:35 +0800

    example: react-compont add test case.

---
+++ example/react-component/src/index.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import Button from './';
+
+
+
+describe('<Badge />', () => {
+  it('Should output a Badge', () => {
+    const component = TestRenderer.create(
+      <Button type="danger">BUTTON</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    if (tree) {
+      expect(tree.type).toBe('button');
+      expect(tree.props.className).toBe('w-btn w-btn-size-default w-btn-danger');
+      expect(tree.children).toHaveLength(1);
+      if (tree.children) {
+        expect(tree.children[0].type).toBe('span');
+      }
+    }
+  });
+});
