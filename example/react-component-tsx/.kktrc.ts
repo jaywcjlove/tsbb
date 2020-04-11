5f27c588035f3a992198552ead1d2597082651df
jaywcjlove <398188662@qq.com>
2020-04-12 01:14:17 +0800

    fix(react-component-tsx): Update kkt config.

--- example/react-component-tsx/.kktrc.ts
+++ example/react-component-tsx/.kktrc.ts
@@ -1,10 +1,14 @@
 import path from 'path';
+import { OptionConf } from 'kkt';
+import webpack from 'webpack';
+
+type Webpack = typeof webpack;
 
 export const loaderOneOf = [
   require.resolve('@kkt/loader-less')
 ];
 
-export default (conf, opts, webpack) => {
+export default (conf: webpack.Configuration, opts: OptionConf, webpack: Webpack) => {
   const pkg = require(path.resolve(process.cwd(), 'package.json'));
   // conf.module.rules.map((item) => {
   //   if (item.oneOf) {
@@ -16,7 +20,7 @@ export default (conf, opts, webpack) => {
   //   return item;
   // });
   // 获取版本
-  conf.plugins.push(
+  conf.plugins!.push(
     new webpack.DefinePlugin({
       VERSION: JSON.stringify(pkg.version),
     })
