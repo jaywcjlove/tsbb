1c2f6b26add2d236abc5f06cbe5b13dfdef2c53a
jaywcjlove <398188662@qq.com>
2020-04-19 02:42:06 +0800

    chore: update kkt config.

--- example/react-component-tsx/.kktrc.ts
+++ example/react-component-tsx/.kktrc.ts
@@ -10,15 +10,6 @@ export const loaderOneOf = [
 
 export default (conf: webpack.Configuration, opts: OptionConf, webpack: Webpack) => {
   const pkg = require(path.resolve(process.cwd(), 'package.json'));
-  // conf.module.rules.map((item) => {
-  //   if (item.oneOf) {
-  //     item.oneOf.unshift({
-  //       test: /\.md$/,
-  //       use: require.resolve('raw-loader'),
-  //     });
-  //   }
-  //   return item;
-  // });
   // 获取版本
   conf.plugins!.push(
     new webpack.DefinePlugin({
