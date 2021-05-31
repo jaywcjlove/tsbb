95141d983958992c9760a0795552dd1bc4597255
jaywcjlove <398188662@qq.com>
2021-05-31 12:49:10 +0800

    fix: Fix yargs v17 upgrade error.

--- packages/tsbb/src/command/tsbb.ts
+++ packages/tsbb/src/command/tsbb.ts
@@ -2,25 +2,27 @@
 
 import yargs from 'yargs';
 
-const command = yargs
-  .usage('Usage: $0 [options]')
-  .command(require('./create'))
-  .command(require('../build'))
-  .command(require('../build/watch'))
-  .command(require('../types'))
-  .command(require('./test'))
-  .example(`\n$\x1b[1;32m tsbb build\x1b[0m`, '\nBuild your project once and exit.')
-  .example(`$\x1b[1;32m tsbb watch\x1b[0m`, 'Rebuilds on any change.')
-  .example(`$\x1b[1;32m tsbb test\x1b[0m`, 'Run test suites related.')
-  .example(`$\x1b[1;32m tsbb test --coverage\x1b[0m`, 'Test coverage information should be collected')
-  .updateStrings({
-    // 'Commands:': 'Commands:\n',
-    // 'Options:': 'Options:\n',
-  })
-  .help()
-  .locale('en')
-  .epilog('Copyright 2019 \n').argv;
+(async () => {
+  const argv = await yargs(process.argv.slice(2))
+    .usage('Usage: $0 [options]')
+    .command(require('./create'))
+    .command(require('../build'))
+    .command(require('../build/watch'))
+    .command(require('../types'))
+    .command(require('./test'))
+    .example(`\n$\x1b[1;32m tsbb build\x1b[0m`, '\nBuild your project once and exit.')
+    .example(`$\x1b[1;32m tsbb watch\x1b[0m`, 'Rebuilds on any change.')
+    .example(`$\x1b[1;32m tsbb test\x1b[0m`, 'Run test suites related.')
+    .example(`$\x1b[1;32m tsbb test --coverage\x1b[0m`, 'Test coverage information should be collected')
+    .updateStrings({
+      // 'Commands:': 'Commands:\n',
+      // 'Options:': 'Options:\n',
+    })
+    .help()
+    .locale('en')
+    .epilog('Copyright 2019 \n').argv;
 
-if (command._.length === 0) {
-  yargs.help().showHelp();
-}
+  if (argv._.length === 0) {
+    yargs.help().showHelp();
+  }
+})();
