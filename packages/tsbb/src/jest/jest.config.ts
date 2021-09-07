30e44e7f087120fc4c09752364158a15ef23eeac
jaywcjlove <398188662@qq.com>
2021-09-08 00:00:21 +0800

    fix(tsbb): Update testMatch feild in jest config.

--- packages/tsbb/src/jest/jest.config.ts
+++ packages/tsbb/src/jest/jest.config.ts
@@ -9,7 +9,11 @@ export default (resolve: Function, rootDir: string) => {
   const conf: IJestConfig = {
     rootDir: rootDir,
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
-    testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+    testMatch: [
+      '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+      '<rootDir>/test/**/*.{js,jsx,ts,tsx}',
+      '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    ],
     testEnvironment: 'jsdom',
     testRunner: require.resolve('jest-circus/runner'),
     transform: {
