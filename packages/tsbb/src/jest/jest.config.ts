ffbf9b0a5cdab3ab30b862b7bdb5f426992f5e80
jaywcjlove <398188662@qq.com>
2022-01-09 01:20:53 +0800

    fix(tsbb): Modify jest config.

--- packages/tsbb/src/jest/jest.config.ts
+++ packages/tsbb/src/jest/jest.config.ts
@@ -1,13 +1,27 @@
 import path from 'path';
 import Jest from '@jest/types';
 
+const moduleFileExtensions = [
+  'web.mjs',
+  'mjs',
+  'web.js',
+  'js',
+  'web.ts',
+  'ts',
+  'web.tsx',
+  'tsx',
+  'json',
+  'web.jsx',
+  'jsx',
+];
+
 interface IJestConfig extends Jest.Config.InitialOptions {
   [key: string]: any;
 }
 
 export default (resolve: Function, rootDir: string) => {
   const conf: IJestConfig = {
-    rootDir: rootDir,
+    roots: ['<rootDir>/src'],
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
     testMatch: [
       '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
@@ -15,7 +29,6 @@ export default (resolve: Function, rootDir: string) => {
       '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
     ],
     testEnvironment: 'jsdom',
-    testRunner: require.resolve('jest-circus/runner'),
     transform: {
       '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': resolve('lib/jest/babelTransform.js'),
       '^.+\\.css$': resolve('lib/jest/cssTransform.js'),
@@ -27,12 +40,16 @@ export default (resolve: Function, rootDir: string) => {
     ],
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
-      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+      '^.+\\.module\\.(css|less|styl|sass|scss)$': 'identity-obj-proxy',
     },
+    moduleFileExtensions: [...moduleFileExtensions, 'node'].filter((ext) => !ext.includes('mjs')),
     watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
     resetMocks: true,
     coverageReporters: ['lcov', 'json-summary'],
   };
+  if (rootDir) {
+    conf.rootDir = rootDir;
+  }
 
   const overrides: IJestConfig = Object.assign({}, require(path.join(rootDir, 'package.json')).jest);
 
@@ -71,15 +88,27 @@ export default (resolve: Function, rootDir: string) => {
     });
     const unsupportedKeys = Object.keys(overrides);
     if (unsupportedKeys.length) {
-      console.error(
-        '\x1b[1;31mOut of the box, TSBB only supports overriding \x1b[0m' +
-          '\x1b[1;31mthese Jest options:\x1b[0m\n\n' +
-          supportedKeys.map((key) => '  \u2022 ' + key).join('\n') +
-          '.\n\n' +
-          '\x1b[1;31mThese options in your package.json Jest configuration \x1b[0m' +
-          '\x1b[1;31mare not currently supported by TSBB App:\x1b[0m\n\n' +
-          unsupportedKeys.map((key) => '  \u2022 ' + key).join('\n'),
-      );
+      const isOverridingSetupFile = unsupportedKeys.indexOf('setupFilesAfterEnv') > -1;
+      if (isOverridingSetupFile) {
+        console.error(
+          '\x1b[0;31mWe detected ' +
+            '\x1b[4;31msetupFilesAfterEnv \x1b[0;31m' +
+            'in your package.json.\n\n' +
+            'Remove it from Jest configuration, and put the initialization code in ' +
+            '\x1b[4;31msrc/setupTests.js\x1b[0;31m' +
+            '.\nThis file will be loaded automatically.\x1b[0m\n',
+        );
+      } else {
+        console.error(
+          '\x1b[1;31mOut of the box, TSBB only supports overriding \x1b[0m' +
+            '\x1b[1;31mthese Jest options:\x1b[0m\n\n' +
+            supportedKeys.map((key) => '  \u2022 ' + key).join('\n') +
+            '.\n\n' +
+            '\x1b[1;31mThese options in your package.json Jest configuration \x1b[0m' +
+            '\x1b[1;31mare not currently supported by TSBB App:\x1b[0m\n\n' +
+            unsupportedKeys.map((key) => '  \u2022 ' + key).join('\n'),
+        );
+      }
       process.exit(1);
     }
   }
