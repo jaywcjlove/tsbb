451cf9b24d0a21d255dc1c982c9254c6da2d8741
jaywcjlove <398188662@qq.com>
2023-03-28 21:57:16 +0800

    test: fix test case error.

--- examples/typenexus/test/main.test.ts
+++ examples/typenexus/test/main.test.ts
@@ -3,23 +3,26 @@ import supertest from 'supertest';
 import { UserController } from '../src/UserController';
 
 describe('API request test case', () => {
-  const app = new TypeNexus(3002, {
-    routePrefix: '/api',
-    developmentMode: false,
-  });
+  let app: TypeNexus
+  beforeAll(() => {
+    app = new TypeNexus(3002, {
+      routePrefix: '/api',
+      developmentMode: false,
+    });
+  
+    app.authorizationChecker = async (action: Action, roles: string[]) => {
+      return false;
+    }
+  })
 
-  app.authorizationChecker = async (action: Action, roles: string[]) => {
-    return false;
-  }
-
-  app.controllers([UserController]);
   test('GET /api/questions', async () => {
+    app.controllers([UserController]);
     let result = await supertest.agent(app.app)
       .get('/api/questions')
       .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .expect('Content-Type', /json/)
       .expect(401)
-  
     expect(result.body.name).toEqual('AuthorizationRequiredError');
     expect(Object.keys(result.body)).toEqual([ 'name', 'message' ]);
   });
