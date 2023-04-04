7a0642123afac6b47939f743667cf3dcca1b1aad
jaywcjlove <398188662@qq.com>
2023-04-04 10:01:18 +0800

    test: update transform file options.

--- packages/jest/src/transform/babel.ts
+++ packages/jest/src/transform/babel.ts
@@ -1,5 +1,18 @@
 import babelJest from 'babel-jest';
 
+const hasJsxRuntime = (() => {
+  if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
+    return false;
+  }
+
+  try {
+    require.resolve('react/jsx-runtime');
+    return true;
+  } catch (e) {
+    return false;
+  }
+})();
+
 export default babelJest.createTransformer({
   presets: [
     [
@@ -11,7 +24,18 @@ export default babelJest.createTransformer({
       },
     ],
     require('@babel/preset-typescript').default,
-    require('@babel/preset-react').default,
+    [
+      require('@babel/preset-react').default,
+      {
+        // Adds component stack to warning messages
+        // Adds __self attribute to JSX which React will use for some warnings
+        development: true,
+        // Will use the native built-in instead of trying to polyfill
+        // behavior for any plugins that require one.
+        ...(hasJsxRuntime ? {} : { useBuiltIns: true }),
+        runtime: hasJsxRuntime ? 'automatic' : 'classic',
+      },
+    ],
   ],
   plugins: [
     require('@babel/plugin-transform-runtime').default,
@@ -20,8 +44,23 @@ export default babelJest.createTransformer({
      * https://github.com/facebook/jest/issues/12183#issuecomment-1004320665
      */
     require('babel-plugin-transform-import-meta').default,
-    [require('@babel/plugin-proposal-decorators').default, { version: 'legacy' }],
+    [require('@babel/plugin-proposal-decorators').default, { legacy: true }],
+    [require('@babel/plugin-proposal-class-properties').default, { loose: true }],
+    [
+      require('@babel/plugin-proposal-private-methods').default,
+      {
+        loose: true,
+      },
+    ],
+    [
+      require('@babel/plugin-proposal-private-property-in-object').default,
+      {
+        loose: true,
+      },
+    ],
     require('@babel/plugin-proposal-object-rest-spread').default,
+    // Adds Numeric Separators
+    require('@babel/plugin-proposal-numeric-separator').default,
   ],
   babelrc: false,
   configFile: false,
